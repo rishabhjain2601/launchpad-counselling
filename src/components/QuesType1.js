eda66d1f20fc74e706b6b12116485c73e68cee2a
rishabhjain2601 <rishabhjain2dtu@gmail.com>
2023-09-11 21:16:54 +0530

    fixes

--- src/components/QuesType1.js
+++ src/components/QuesType1.js
@@ -1,5 +1,6 @@
-import React,{ useState }  from 'react'
+import React, { useState } from 'react'
 import Button from "../utils/Button";
+
 
 const QuesType1 = () => {
   const [ans1, setAns1] = useState([]);
@@ -14,10 +15,23 @@ const QuesType1 = () => {
       <div>
         Q. I want to pursue career related to ______
       </div>
+{/* <<<<<<< HEAD
       <form action="" className='mx-auto'>
-        <textarea name="" id="" className='text-black outline-white sbar pl-2 w-[90%]  ml-[3.5%] h-[260px] my-4 rounded-2xl resize-none overflow-y-scroll '/>
-        <button className='bg-gray-600 rounded ml-[3.5%] py-1 px-2'>Previous</button>
-        <button className='float-right bg-gray-600 rounded py-1 px-4 mr-[6.5%]'>Next</button>
+        <textarea name="" id="" className='text-black outline-white sbar pl-2 w-[90%]  ml-[3.5%] h-[260px] my-4 rounded-2xl resize-none overflow-y-scroll ' onChange={handleChange} />
+        <button className='bg-gray-600 rounded ml-[3.5%] py-1 px-2' onClick={setAns1(['', -1])}>Previous</button>
+        <button className='float-right bg-gray-600 rounded py-1 px-4 mr-[6.5%]' disabled={!ans1} onClick={setAns1([`${ans1}`, 1])}>Next</button>
+======= */}
+      <form action="" className="mx-auto">
+        <textarea
+          name=""
+          id=""
+          className="text-black outline-white pl-2 w-full h-[260px] my-4 rounded-2xl resize-none overflow-y-scroll "
+        />
+        <div className="flex justify-between mt-5">
+          <Button>Previous</Button>
+          <Button>Next</Button>
+        </div>
+{/* >>>>>>> 67955863f00023a8a019b97b1dcdc5eb7dc5b667 */}
       </form>
     </div>
   );
